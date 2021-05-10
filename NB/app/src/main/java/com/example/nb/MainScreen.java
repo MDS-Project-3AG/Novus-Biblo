ecf62822ef623734a21b0bf479717ef8490bf3e1
Andreea <andreea_popa22@yahoo.ro>
2021-05-10 20:14:19 +0300

    eroare login

--- NB/app/src/main/java/com/example/nb/MainScreen.java
+++ NB/app/src/main/java/com/example/nb/MainScreen.java
@@ -1,13 +1,13 @@
-package com.example.nb;
+package com.example.nb.data;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Log extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log);
+        setContentView(R.layout.activity_main_screen);
     }
 }
