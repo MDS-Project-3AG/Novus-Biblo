4f79909e1d3c840f30b1a0cd77a9e30b45065321
Andreea <andreea_popa22@yahoo.ro>
2021-06-12 20:11:28 +0300

    create bookshelf bug

--- NB/app/src/main/java/com/example/nb/MyList.java
+++ NB/app/src/main/java/com/example/nb/MyList.java
@@ -1,5 +1,7 @@
 package com.example.nb;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.StrictMode;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
+
+import androidx.core.app.ActivityCompat;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -27,23 +31,22 @@ public class MyList extends AppCompatActivity {
     private TextView textView;
     FirebaseAuth fAuth;
 
-    public MyList(){};
+    //public MyList(){};
 
-    public MyList(int user_id, List<BookDetails> book_list) {
-        this.user_id = user_id;
-        this.book_list = book_list;
-    }
+//    public MyList(int user_id, List<BookDetails> book_list) {
+//        this.user_id = user_id;
+//        this.book_list = book_list;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        textView = findViewById(R.id.textView4);
 
         fAuth = FirebaseAuth.getInstance();
         String currentuser = fAuth.getCurrentUser().getUid();
-        System.out.println("Yeeeeey");
-        System.out.println(currentuser);
-
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
